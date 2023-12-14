d7ea1135ad3b9cb1f38c87cf7dd138e028709cbb
fastoch <fastoch@ik.me>
2023-12-14 18:09:32 +0100

    chapter 11 main.ts 4:39:00

--- list_app/src/main.ts
+++ list_app/src/main.ts
@@ -1,4 +1,18 @@
 import './css/style.css'
+import ListItem from './model/ListItem'
+import FullList from './model/FullList'
+import ListTemplate from './templates/ListTemplate'
 
+const initApp = ():void => {
+  const fullList = FullList.instance
+  const template = ListTemplate.instance
 
+  // load the form that allows user to add items to the list
+  const itemEntryForm = document.getElementById('itemEntryForm') as HTMLFormElement
+  itemEntryForm.addEventListener('submit', () => {
+    
+  })
+}
 
+// We're not going to run our javascript until the page is loaded
+document.addEventListener('DOMContentLoaded', initApp);
--- list_app/src/templates/ListTemplate.ts
+++ list_app/src/templates/ListTemplate.ts
@@ -8,9 +8,10 @@ interface DOMList {
 
 export default class ListTemplate implements DOMList {
 
-  static instance: ListTemplate = new ListTemplate() // creates a singleton
-
   ul: HTMLUListElement
+
+  static instance: ListTemplate = new ListTemplate() // creates a singleton
+  // when we refer to this singleton, it will be with ListTemplate.instance
 
   private constructor() {
     this.ul = document.getElementById("listItems") as HTMLUListElement
@@ -23,14 +24,47 @@ export default class ListTemplate implements DOMList {
 
   render(fullList: FullList): void {
     this.clear() // clear what we already have so we don't duplicate the list
+    
     fullList.list.forEach(item => {
+      // add a list item
       const li = document.createElement("li") as HTMLLIElement
       li.className = "item"
 
+      // add a checkbox to the list item
       const check = document.createElement("input") as HTMLInputElement
       check.type = "checkbox"
+      check.id = item.id 
+      // check.tabIndex = 0 // this is optional
+      check.checked = item.checked
+      li.append(check)
 
+      // update the checkbox state and save the list state
+      check.addEventListener('change', () => {
+        item.checked = !item.checked
+        fullList.save()
+      })
+
+      // add a label to the list item
+      const label = document.createElement("label") as HTMLLabelElement
+      label.htmlFor = item.id
+      label.textContent = item.item
+      li.append(label)
+
+      // add a button to the list item
+      const button = document.createElement("button") as HTMLButtonElement
+      button.className = "button"
+      button.textContent = "X"
+      li.append(button)
       
+      // delete the list item when the button is clicked
+      button.addEventListener('click', () => {
+        fullList.removeItem(item.id)
+        // no need to save since removeItem() does it
+        this.render(fullList) // item removed from the list remains visible
+      })
+
+      this.ul.append(li) // finally, add each list item to the list
+
     })
   }
 }
